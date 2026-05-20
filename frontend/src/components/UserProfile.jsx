034e69bac5478f0a30e7ef7ae81b445739dbdb81
Tharunmudrakola <tharunmudrakola2004@gmail.com>
2026-05-20 14:41:10 +0530

    fixed userprofile toast import

--- frontend/src/components/UserProfile.jsx
+++ frontend/src/components/UserProfile.jsx
@@ -1,6 +1,5 @@
 import { useAuth } from "../store/authStore";
 import { useNavigate } from "react-router";
-import { toast } from "react-hot-toast";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import toast from "react-hot-toast";
